e55deb33acd55f3a9ae632d83623ca64b928cddd
YoungHo Park <youngpumpkin@gmail.com>
2020-06-06 10:15:53 +0000

    paging view added

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,63 @@
 module.exports = {
     getHomePage: (req, res) => {
+		var page_num = 1;
+			if (typeof req.query.page_num !== 'undefined') {
+				page_num = Number(req.query.page_num);
+			}
+		
+		const item_num = 1;
+		const page_size = 10;
+		const skip_size = (page_num - 1) * item_num;
+		const page_query = "LIMIT " + item_num + " OFFSET " + skip_size;
+		var page_start = 1;
+		var page_end = 1;
+		var page_total = 1;
+		var total_count = 1;
 
-       	let query = "SELECT * FROM players ORDER BY id ASC"; // query database to get all the players
+		db.query("SELECT COUNT(*) FROM players", (err, result) => {				
+			total_count = Number(result.rows[0].count);
+					    console.log(total_count)
 
+			page_total = Math.ceil(total_count / item_num); 
+						console.log("page total" + page_total)
+
+    		page_start = ((Math.ceil(page_num / page_size) - 1) * page_size) + 1;
+			page_end = (page_start + page_size) - 1;
+
+			
+			if (page_end > page_total) 
+				page_end = page_total;
+
+			
+		});
+
+       	let query = "SELECT * FROM players ORDER BY id ASC " + page_query; 
+		
 		if (req.path == '/search') {
 			let username = req.query.username
 			let reg_date = req.query.reg_date
-       		query = "SELECT * FROM players where user_name like '%" + username + "%' and reg_time::date = date '" + reg_date + "' ORDER BY id ASC";
+       		query = "SELECT * FROM players WHERE user_name like '%" + username + "%' \
+						AND reg_time::date = date '" + reg_date + "' ORDER BY id ASC " + page_query;
 		}
-        // execute query
+		
+		
     	db.query(query, (err, result) => {
             if (err) {
                 res.redirect('/');
             }
-
+			
+			const page_info = {
+				num: page_num,
+				start: page_start,
+				end: page_end,
+				total: page_total
+			}
+			
             res.render('index.ejs', {
                 title: "Welcome to Socka | View Players"
                 ,players: result
 				,query: req.query
+				,page: page_info
             });
         });
     },
